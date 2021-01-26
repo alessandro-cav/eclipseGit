18b4b0e6a2dd2fe71e285844956edbd460ceda1d
Alessandro Agusto <alessandro-cav@hotmail.com>
2021-01-26 20:37:31 -0300

    Adicionando projeto no github

---
+++ demo/src/main/java/com/example/demo/teste/Teste.java
@@ -0,0 +1,11 @@
+package com.example.demo.teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("testando o git");
+	}
+
+}
